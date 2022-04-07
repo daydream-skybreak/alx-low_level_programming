056f594b9a8d0ba63fa0d1cd6ed397a2b0859e1b
Adonay <samueldesta323@gmail.com>
2022-04-07 03:36:09 -0700

    this is my fifth task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - prints message using printf
+ *
+ * Return: 0 after execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
