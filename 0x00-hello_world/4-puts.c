b5eeebcf3c9209101ebfceabc41440256c428191
Adonay <samueldesta323@gmail.com>
2022-04-07 03:26:58 -0700

    commits my first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/*
- * main this is ths first c program
+/**
+ * main -  this is ths first c program
  * 
  * returns 0
  */
-int main()
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
