b4a6f7e2d17d7e70089644031f54cf2961d3571d
Adonay <samueldesta323@gmail.com>
2022-04-08 12:19:54 -0700

    advanced task3 completed

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,11 +12,11 @@ while (num1 <=98)
 {
 	while (num2 <= 99)
 	{
-		putchar((num1 / 10) + '0';
-		putchar((num1 % 10) + '0';
+		putchar((num1 / 10) + '0');
+		putchar((num1 % 10) + '0');
 		putchar(' ');
-		putchar((num2 / 10) + '0';
-		putchar((num2 % 10) + '0';
+		putchar((num2 / 10) + '0');
+		putchar((num2 % 10) + '0');
 
 		if (num1 == 98 && num2 == 99)
 		continue;
