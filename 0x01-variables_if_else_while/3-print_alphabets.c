9649bc27b7845b0e29180006c412b70263586624
Adonay <samueldesta323@gmail.com>
2022-04-08 08:40:35 -0700

    3rd task completed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - outputs lowercase of alphabet entered
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+int val = 97;
+while (val < 123)
+        putchar(val++);
+val = 65;
+while (val < 90)
+        putchar(val++);
+putchar('\n');
+return (0);
+}
+
