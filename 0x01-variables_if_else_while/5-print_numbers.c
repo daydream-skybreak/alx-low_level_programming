de99e9e04306adeb4b76e33965ca7d81472507f5
Adonay <samueldesta323@gmail.com>
2022-04-08 09:06:09 -0700

    5th task completed

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,14 @@
 /**
  * main - outputs numbers
  *
- * Returns: 0 after successful completion
+ * Return: 0 after successful completion
  */
 int main(void)
 {
-	int val = 0;
-	while (val < 10)
-		printf("%d", val++);
-	putchar('\n');
-	return (0);
-}	
+int val = 0;
+
+while (val < 10)
+	printf("%d", val++);
+putchar('\n');
+return (0);
+}
