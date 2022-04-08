c0c1546d65d76886e4575b61babe8dff8948a15d
Adonay <samueldesta323@gmail.com>
2022-04-08 10:10:05 -0700

    advanced task 1 completed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,12 +15,12 @@ while(val1<58)
 {
 	putchar(val);
 	putchar(val1);
-	if (val == 56 && val1=57)
+	if (val == 56 && val1 == 57)
 		break;
 	putchar(',');
 	putchar(' ');
 }
-if (val == 56 && val1=57)
+if (val == 56 && val1 == 57)
 break;
 val++;
 
