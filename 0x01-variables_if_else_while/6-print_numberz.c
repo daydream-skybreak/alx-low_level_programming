59393af14ff5bb0998b92b1857cfcc650e43911d
Adonay <samueldesta323@gmail.com>
2022-04-08 09:14:50 -0700

    6th task completed

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints 0-9 numbers using putchar
+ *
+ * Return : 0 after successful execution
+ */
+int main(void)
+{
+int val = 48;
+while (val < 58)
+	putchar(val++);
+putchar('\n');
+return (0);
+}
