1fa546dcf648c7a353c3c72d8a8bcc8691f297d7
Adonay <samueldesta323@gmail.com>
2022-04-08 14:06:48 -0700

    idk what is worng

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,29 +6,26 @@
  */
 int main(void)
 {
-int num1 = 0;
+int num1, num2;
 
-while (num1 <= 98)
+for (num1 = 0; num1 <= 98; nmu1++)
 {
-	int num2 = num1 + 1;
+for (num2 = num1 + 1; num2<=99; num2++)
+{
+putchar((num1 / 10) + '0');
+putchar((num1 % 10) + '0');
+putchar(' ');
+putchar((num2 / 10) + '0');
+putchar((num2 % 10) + '0');
 
-	while (num2 <= 99)
-	{
-		putchar((num1 / 10) + '0');
-		putchar((num1 % 10) + '0');
-		putchar(' ');
-		putchar((num2 / 10) + '0');
-		putchar((num2 % 10) + '0');
+if (num1 == 96 && num2 == 99)
+continue;
 
-		if (num1 == 98 && num2 == 99)
-		continue;
-
-		putchar(',');
-		putchar(' ');
-		num2++;
-	}
-num1++;
+putchar(',');
+putchar(' ');
 }
+}
+
 putchar('\n');
 
 return (0);
