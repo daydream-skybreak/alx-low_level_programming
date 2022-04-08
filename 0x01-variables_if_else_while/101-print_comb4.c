9e58fa03c522cefd3dff511abeb8bb8113132cde
Adonay <samueldesta323@gmail.com>
2022-04-08 10:46:18 -0700

    advaced task 2 completed

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - outputs lowercase of alphabet entered
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+int val = 48, i = 1;
+
+while (i < 10)
+{
+int val1 = 48 + i;
+while (val1 < 58)
+{
+int val2 = 49 + i;
+	while ( val2 < 58)
+{
+	putchar(val);
+	putchar(val1);
+	putchar(val2++);
+	if (val == 55 && val1 == 56 && val2 == 58)
+		break;
+	putchar(',');
+	putchar(' ');
+}
+val2++;
+}
+if (val == 55 && val1 == 56)
+break;
+val++;
+i++;
+}
+putchar('\n');
+return (0);
+}
