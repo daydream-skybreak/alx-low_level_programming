8f2a8dad3073549c0b9c2e6b28b2e6279ba5b236
Adonay <samueldesta323@gmail.com>
2022-04-08 08:30:12 -0700

    2nd task is done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - outputs lowercase of alphabet entered
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+int val = 97;
+while (val<123)
+	putchar(val++);
+return (0);
+}
