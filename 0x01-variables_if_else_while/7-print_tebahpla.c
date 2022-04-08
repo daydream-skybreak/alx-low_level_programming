cfcc9d00d41ff8db1c539f4b37492cc48db16bb8
Adonay <samueldesta323@gmail.com>
2022-04-08 09:21:27 -0700

    7th task complete

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - outputs lowercase of alphabet entered
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+int val = 122;
+
+while (val > 98)
+        putchar(val--);
+putchar('\n');
+return (0);
+}
