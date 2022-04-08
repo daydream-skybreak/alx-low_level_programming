a00e56b79946cbf7cd608ba55e0ec312f18d8536
Adonay <samueldesta323@gmail.com>
2022-04-08 06:15:27 -0700

    my 0th task completed

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - checks if the variable n is positive or negative
+ *
+ * Return: 0 after successful exectuion
+ */
+int main(void)
+{
+if(n>0)
+	printf("is positive");
+else if(n==0)
+	printf("is zero");
+else
+	printf("is negative");
+return (0);
+}
