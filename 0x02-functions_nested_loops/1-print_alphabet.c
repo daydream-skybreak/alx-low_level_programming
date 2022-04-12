7af56281f27005ae660eca84cf3466c7f23b3f23
Adonay <samueldesta323@gmail.com>
2022-04-12 10:58:47 -0700

    task 1 complete

---
+++ 0x02-functions_nested_loops/1-print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - lists alll the small alphabet letters
+ * @void - no parameter
+ *
+ * Return: 0 after successsful  compilation
+ */
+void print_alphabet(void)
+{
+	int no = 97;
+
+	int i;
+
+	for (i=0; i<26; i++)
+	{
+		_putchar(no + i);
+	}
+	_putchar('\n');
+	return (0);
+}
