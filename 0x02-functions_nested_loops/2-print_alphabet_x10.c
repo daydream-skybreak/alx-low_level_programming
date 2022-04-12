279dc0e582a0c158e96b7fb383a0f4cef1927017
Adonay <samueldesta323@gmail.com>
2022-04-12 11:35:14 -0700

    task 2 done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - start of execution
+ * @void - no parameter
+ *
+ * Return: returns nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	int val = 97
+
+	for (i = 0; i<10; i++)
+	{
+		for (j = 0; j<26; j++)
+		{
+			_putchar(val + i);
+		}
+		putchar('\n');
+	}
+}
