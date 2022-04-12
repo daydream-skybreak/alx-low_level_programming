38151131b582ebe2dadd8290e0348c3f4be66d4d
Adonay <samueldesta323@gmail.com>
2022-04-12 13:28:25 -0700

    task 10 completed

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
- * Return: Always 0
+ * add - adds two numbers
+ * @a: integer paremeter 1
+ * @b: integer parameter 2
+ * Return: the summation
  */
 int add(int a, int b)
 {
