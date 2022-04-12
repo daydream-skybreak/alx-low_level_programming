401421d89bd6d29647e96f6cb2f70b8adf5aa411
Adonay <samueldesta323@gmail.com>
2022-04-12 13:25:31 -0700

    task 9 done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * times_table - displays the 9th time table
+ * @void: no parameter
+ *
+ * Return: nothing to return
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int b = i * j;
+			_putchar(b, '0');
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
+
