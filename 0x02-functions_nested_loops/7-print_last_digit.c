4ac05ee550dfd973a925c1a8a4c9fe866ef882c2
Adonay <samueldesta323@gmail.com>
2022-04-12 13:13:18 -0700

    task 7 done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,10 @@
  * Return: returns hte last digit of the passed argument
  */
 int print_last_digit(int n)
-{
-	return ((n % 10 * 10) + n % 10);
+{	int ln = n % 10;
+	
+	if (ln < 0)
+		ln *= -1;
+	_putchar(ln + '0');
+	return (ln);
 }
