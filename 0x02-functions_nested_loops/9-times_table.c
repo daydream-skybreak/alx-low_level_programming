6a73502af60d5685db1adb76f587f131d2a67d3c
Adonay <samueldesta323@gmail.com>
2022-04-12 13:52:42 -0700

    task 9 completed

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,6 +14,7 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			int k = i * j;
+
 			if (j == 0)
 			{
 				_putchar(k + '0');
@@ -25,13 +26,13 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(k + '0');
 			}
-			else if (k <= 9)
-                        {
+			else if (k >= 10)
+			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(k / 10 + '0');
 				_putchar(k % 10 + '0');
-                        }
+			}
 		}
 		_putchar('\n');
 	}
