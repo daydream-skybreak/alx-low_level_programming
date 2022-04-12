15fa827ec6561b44e1446891a77813e327bd19cc
Adonay <samueldesta323@gmail.com>
2022-04-12 13:02:03 -0700

    task 8 done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * jack_bauer - start execution
+ * @void: no parameter
+ *
+ * Return: 0 after completion
+ */
+void jack_bauer(void)
+{
+	int i = 0, j = 0;
+	while (i < 24)
+	{
+		while(j < 60)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+                        _putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+                        _putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
