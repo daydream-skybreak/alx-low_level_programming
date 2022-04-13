2096102e91473328eb73f7ba7a87eb5c51ba091a
Adonay <samueldesta323@gmail.com>
2022-04-13 08:24:07 +0300

    advanced task 1 done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,19 +8,37 @@
  */
 void times_table(int n)
 {
-	int i, j;
+	int num, mult, prod;
 
-	for (i = 0; i < = n; i++)
+	if (num <+ 15 && num >= 0)
 	{
-		for (j = 0; j <= n; j++)
+		for (num = 0; num <= n ; num++)
 		{
-			int k = i * j;
-			printf("%d", k);
-			if (j != n)
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
 			{
-				printf(",...");
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
 	}
-	pinrtf("\n");
 }
