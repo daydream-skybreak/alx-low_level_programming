bcd8944526fd265ef9428a4b63256dd557c848ac
Adonay <samueldesta323@gmail.com>
2022-04-13 13:08:24 +0300

    task 0 done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * main - start function
- *
  * Return: 0
  */
-int main (void)
-{
-int i;
-i = 98;
 
-positive_or_negative(i);
-return (0);
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
 }
