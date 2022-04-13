5ace14925e5080eb3ac695f3ff5e0be8d647c6e2
Adonay <samueldesta323@gmail.com>
2022-04-13 11:01:54 +0300

    advanced task 4 completed

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start execution
+ * @void: no parameter
+ *
+ * Return: 0 after compeltion
+ */
+int main(void)
+{
+	long int fst = 0, snd = 1;
+	long int sum = 0, total = 0;
+	while (sum < 4000000)
+	{
+		sum = fst + snd;
+		fst = snd;
+		snd = sum;
+		if (sum % 2 == 0)
+		{
+			total = total + sum;
+		}
+	}
+	printf("%ld\n", total);
+	return (0);
+}
