e9f34641b1edcbfb5a8617e63947e651ff763f0f
Adonay <samueldesta323@gmail.com>
2022-04-13 08:47:57 +0300

    advanced task 2 completed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-	int num;
+	int num, sum = 0;
 	
-	for (num = 1; num < 1024; num++);
-	{
-		if (num % 3 == 0 || num % 5 == 0)
-		{
-			printf("%d \n", num);
-		}
-	}
+for (num = 1; num < 1024; num++);
+{
+if (num % 3 == 0 || num % 5 == 0)
+{
+sum += num;
+}
+printf("%d \n", sum);
+}
 }
