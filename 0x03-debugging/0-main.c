c8cc462bcc7d73ea92e4a03f7dcfe420d7518eb2
Adonay <samueldesta323@gmail.com>
2022-04-13 12:56:11 +0300

    my commit message

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - start function
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	int i;
+	i = 0;
+
+	positive_or_negative();
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int print_remaining_days(int m, int d, int y);
+int convert_day(int m, int d);
+
+#endif
