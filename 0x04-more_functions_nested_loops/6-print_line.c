aa2a222240b76a4bc2626e7560853fe797116beb
Adonay <samueldesta323@gmail.com>
2022-04-15 00:22:01 +0300

    task 6 done

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,20 +9,14 @@
 
 void print_lines(int n)
 {
-	int i = 95;
-	
 	int j;
 	
-	if (n <= 0)
-	{
-		_putchar('\n');
-	}
-	else
+	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-			_putchar(i);
+			_putchar('_');
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
