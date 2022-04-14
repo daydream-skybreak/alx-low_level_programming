6f193e09ee9a8e0402f77a581d7b62cd25eacaf6
Adonay <samueldesta323@gmail.com>
2022-04-14 23:01:56 +0300

    i made a change in mian.h file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,4 +10,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
+#endif
