053735709e9dd74cc83b28b54057d5cc1d861775
Adonay <samueldesta323@gmail.com>
2022-04-15 13:09:44 +0300

    advanced task 2 done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_number - prints integers with _putchar
+ * @n: integer parameter
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -n;
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}
