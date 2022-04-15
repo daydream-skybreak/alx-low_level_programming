aa57e4ad340ed878579b567e10f5d63980190a24
Adonay <samueldesta323@gmail.com>
2022-04-15 12:50:14 +0300

    advanced task 1 done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - starts execution
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	unsigned long factor, i, prime, no;
+	no = 612852475143;
+
+	for (prime = 3; prime < no: prime += 2)
+	{
+		while (no % prime ==0)
+		{
+			no = no / prime;
+			factor = prime;
+		}
+	}
+	printf("%ld\n",factor);
+}
