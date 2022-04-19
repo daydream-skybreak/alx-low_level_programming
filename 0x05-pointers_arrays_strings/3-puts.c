ebb98d6bc36d97fc28235c42c058ce3fc4b64008
Adonay <samueldesta323@gmail.com>
2022-04-19 13:29:26 +0300

    tsk 3 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - prints string to std out
+ * @str: string parameter
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+		_putchar(str[i]);
+}
