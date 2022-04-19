65a6d234e35d52ccfcd9cbcca55740a9b30962ab
Adonay <samueldesta323@gmail.com>
2022-04-19 13:18:53 +0300

    task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string parameter
+ *
+ * Return: integer
+ */
+int _strlen(char *s)
+{
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
