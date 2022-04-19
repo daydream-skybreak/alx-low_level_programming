d69eb9a8bee6f59f007bbd61692e1ab0858545f1
Adonay <samueldesta323@gmail.com>
2022-04-19 19:55:32 +0300

    task 8 done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - prints elements of an array
+ * @a: pointer to the first element of an array
+ * @n: size of the array
+ *
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0, num;
+
+	while (i < n)
+	{
+		num = *a++;
+		printf("%d", num);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
