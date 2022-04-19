28e9f51fcde8aaa5a33e297e2db7223c73835336
Adonay <samueldesta323@gmail.com>
2022-04-19 19:16:50 +0300

    task 6 done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,15 +7,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0, j = 0;
+	int i;
 
-	while (str[j] != '\0')
-		i++;
-
-	while (j < i)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		_putchar(str[j]);
-		j += 2;
+		if (i % 2 == 0)
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
