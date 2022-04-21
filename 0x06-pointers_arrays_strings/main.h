0f90b72991213efc0ac0cce2c6a1b78e3935c772
Adonay <samueldesta323@gmail.com>
2022-04-21 12:31:48 +0300

    modified the headr file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 int _putchar(char s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -6,3 +8,4 @@ int strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+#endif
