dd3f5ccfe4b7f40d3326071b8fc5d3c9e1f599a3
Adonay <samueldesta323@gmail.com>
2022-04-21 12:06:18 +0300

    this gotta be it

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
 int _putchar(char s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
@@ -8,4 +6,3 @@ int strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
-#endif
