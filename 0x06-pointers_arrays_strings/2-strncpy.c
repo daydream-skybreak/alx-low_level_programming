ab5064b7829549a36fe183f8e49788f6acdb7cb4
Adonay <samueldesta323@gmail.com>
2022-04-22 10:35:50 +0300

    task 2 done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncpy - copies string to a specified number
+ * @dest: destination string
+ * @src: source string
+ * @n: number of string
+ *
+ * Return: character
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	i = 0;
+	
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++
+	}
+	dest[i] = '\0';
+	return (dest);
+}
