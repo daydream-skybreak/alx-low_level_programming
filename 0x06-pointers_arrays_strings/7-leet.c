717231d82516afaa15630344afd893771d6adac3
Adonay <samueldesta323@gmail.com>
2022-04-22 15:34:58 +0300

    task 7 done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * leet - encodes a given string
+ * @string: string pointer
+ * Return: translated string
+ */
+char *leet(char *string)
+{
+	int a, b;
+
+	char lower[] = "aeotl";
+	char upper[] = "AEOTL";
+	char num[] = "43071";
+
+	for (a = 0; string[a] != '\0': a++)
+	{
+		for (b = 0; lower[b] != '\0' && upper[0] != '\0'; b++)
+		{
+			if (lower[b] == string[a] || upper[b] == string[a])
+			{
+				string[a] = num[b];
+				break;
+			}
+		}
+	}
+	return (string);
+}
