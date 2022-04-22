4926506d967588679d07424a4b6846536cc5169b
Adonay <samueldesta323@gmail.com>
2022-04-22 11:45:38 +0300

    task 2 done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,15 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	i = 0;
 
-	while (i < n && src[i] != '\0')
+	while (i < n)
+
 	{
 		dest[i] = src[i];
-		i++;
-	}
-
-	while (n > i)
-	{
-		dest[i] = '\0';
 		i++;
 	}
 
