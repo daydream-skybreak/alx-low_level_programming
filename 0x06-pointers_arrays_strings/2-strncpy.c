2d40df6fa10abff878ac7f650a01b567894a8076
Adonay <samueldesta323@gmail.com>
2022-04-22 12:14:09 +0300

    task 2 done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int x;
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
+	{
 		dest[x] = src[x];
-
+	}
 	for(; n > x; x++)
+	{
 		dest[x] = '\0';
+	}
 
 	return (dest);
 }
