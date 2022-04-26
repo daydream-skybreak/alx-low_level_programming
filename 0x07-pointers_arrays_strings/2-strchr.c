5461fc7dd5c5ba67704644f8465196f1a48c0db6
Adonay <samueldesta323@gmail.com>
2022-04-26 16:47:19 +0300

    task 2 done 3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,6 +8,7 @@
 char *_strchr(char *s, char c)
 {
 	int i, j;
+
 	i = 0;
 
 	while (s[i])
@@ -15,7 +16,7 @@ char *_strchr(char *s, char c)
 		i++;
 	}
 
-	for (j = 0; j < i; j++)
+	for (j = 0; j <= i; j++)
 	{
 		if (s[j] == c)
 		{
