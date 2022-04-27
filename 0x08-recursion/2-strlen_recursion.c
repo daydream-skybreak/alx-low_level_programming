c30ab806118e27199510fa15010c399a10b08906
Adonay <samueldesta323@gmail.com>
2022-04-27 21:06:33 +0300

    task 2 done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns string's length
+ * @s: string pointer
+ * Return: integer
+ */
+int _strlen_recursion(char *s)
+{
+	int leng = 0;
+
+	if (*s != '\0')
+	{
+		lent += 1;
+
+		_strlen_recursion(s + 1);
+	}
+	return (leng);
+}
