2e11d2ae04aa7dc9ade5dd74e3c16256f3c95207
Adonay <samueldesta323@gmail.com>
2022-04-27 13:42:18 +0300

    task 6 done

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_chessboard - prints chessboard as the name suggests
+ * @a: a 2 dimensional array
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	for (b = 0; b < 8; b++)
+	{
+		for (c = 0; c < 8; c++)
+			_putchar(a[b][c] + '0')
+	}
+}
