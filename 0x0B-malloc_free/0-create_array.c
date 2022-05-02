8d4be42e799bd7d2d6719136d9b17ba2e9f30a92
Adonay Desta <samueldesta323@gmail.com>
2022-05-02 21:31:09 +0300

    task 0 done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,13 @@
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
+char *s;
 
 if (size == 0)
 {
 	return (NULL);
 }
-char *s;
+
 s = (char*)malloc(size * sizeof(char));
 
 for (i = 0; i < size; i++)
