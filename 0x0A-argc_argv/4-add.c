af126220250ee2fb4b1ff9e2f31e72b272bafe18
Adonay Desta <samueldesta323@gmail.com>
2022-05-03 10:14:22 +0300

    task 4 done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,48 +1,44 @@
-#include <ctype.h>
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-/**
- * main - main function
- * @argc: number of command lines
- * @argv: array of string of command lines
- * Return: 1
- */
-int main(int argc, char const *argv[])
-{
-	int i, sum = 0, checker = 0;
-	
-	if (argc == 1)
-	{
-		printf("%d\n", sum);
-		return (0);
-	}
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "holberton.h"
 
-	for (i = 0; i < argc - 1; i++)
-	{
-		if (!(isdigit(argv[i + 1])))
-		{
-			checker++;
-		}
-	}
-	if (checker == argc)
-	{
-		printf("%d\n", sum);
-	}
-	else if (checker < argc - 1)
-	{
-		printf("Error\n");
-		return (1);
-	}
+/**
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, length, sum;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
 	else
 	{
-		for (i = 0; i < argc- 1; i++)
+		sum = 0;
+		for (i = 1; i < argc; i++)
 		{
-			if (i == 0)
-				continue;
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
 			sum += atoi(argv[i]);
 		}
-		printf("%d\n", sum);
+
+	printf("%d\n", sum);
 	}
-return (0);
+	return (0);
 }
