c21c6d2c9cf4b72c6a60ede7d228f6ac68dd1092
adonay <samueldesta323@gmail.com>
2022-05-09 22:48:19 +0300

    task 0 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,25 @@
-#ifndef FILE_DOG
-#define FILE_DOG
-struct dog {
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * dog - structure named dog
+ * @name: name of the dog
+ * @age:age of the dog
+ * @owner: owner of the dog
+ */
+
+typedef struct dog
+{
 	char *name;
 	char *owner;
 	int age;
-};
-#endif /* _dog_h_ */
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
+
+#endif/* _dog_h_ */
