c2d94c7009f98d381180f3b87351203329217a94
adonay <samueldesta323@gmail.com>
2022-05-09 23:01:25 +0300

    task 1 done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 /**
- * inti_dog - assigns for hte structure
+ * init_dog - assigns for hte structure
  * @d: structure
  * @name: name of the dog
  * @age: age of the dog
@@ -9,7 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if(d)
+	{
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
+	}
 }
