472adf64b2cdb5a2b3b45d3951cff52597fd128f
adonay <samueldesta323@gmail.com>
2022-05-09 23:16:19 +0300

    task 2 done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * print_dog - prints dog attribute
+ * @d: structure
+ */
+void print_dog(struct dog *d)
+{
+	char *str = d->name != NULL? d->name: "(nil)";
+	char *str1 = d->owner != NULL? d->owner: "(nil)";
+	int age1 = d->age != NULL? d->age: -1;
+	
+	printf("Name: %s\n", str);
+	if (age1 == -1)
+	{
+		printf("Age: (nil)\n");
+	}
+	else
+	{
+		printf("Age: %d\n", &age1);
+	}
+	printf("Owner: %s", str1);
+}
