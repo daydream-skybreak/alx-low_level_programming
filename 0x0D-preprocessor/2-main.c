25dd318f0876e92a6a08932663461e8939e4217a
adonay <samueldesta323@gmail.com>
2022-05-09 15:28:17 +0300

    task 2 done

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - where execution starts
+ * @void: nothing
  * Return: 0
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
