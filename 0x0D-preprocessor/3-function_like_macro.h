235fd96b877127f85a4e0e7c759e87c624199dad
adonay <samueldesta323@gmail.com>
2022-05-09 16:19:26 +0300

    task 3 done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,6 @@
-/* function_like_macro_h*/
-#ifndef function_like_macro_h_
-#define function_like_macro_h_
-#define ABS(x) (x) >= (0)? (x): (-1) * (x)
-#endif
+#ifndef _function_like_macro_h_
+#define _function_like_macro_h_
+
+#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
+
+#endif /* _function_like_macro_h_ */
