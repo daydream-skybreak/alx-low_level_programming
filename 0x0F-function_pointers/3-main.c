6d77aac0b006e8ba22ff7a23c93efbaaa5f6c71d
Adonay Desta <samueldesta323@gmail.com>
2022-05-11 10:37:11 -0700

    task 3 done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - main function
+ * @argc: number of arguments passed to the function
+ * @argv: array of argument passed ot the function
+ *
+ * Return: integer
+ */
+int main(int argc, char const *argv)
+{
+	int result;
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[2]);
+	b = atoi(srgv[4]);
+
+	result = (*get_op_func(argv[3]))(a, b);
+	printf("%d\n", result);
+	
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds items
+ * @a: int 1
+ * @b: int 2
+ * Return: added result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts items
+ * @a: int 1
+ * @b: int 2
+ * Return: subtracted result
+ */
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_add - multiplies items
+ * @a: int 1
+ * @b: int 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+/**
+ * op_add - division operator
+ * @a: int 1
+ * @b: int 2
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+        return (a / b);
+}
+/**
+ * op_mod - calculates the remainder
+ * @a: int 1
+ * @b: int 2
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+        {
+                printf("Error\n");
+                exit(100);
+        }
+        return (a + b);
+}
