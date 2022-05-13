39bc5a056ac87769a751844186f2d91518652db9
Adonay Desta <samueldesta323@gmail.com>
2022-05-13 12:31:47 -0700

    task 0 done 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int _putchat(char s);
+int _putchar(char s);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
