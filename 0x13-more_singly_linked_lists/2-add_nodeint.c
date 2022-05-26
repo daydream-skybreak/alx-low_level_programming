5268fa254028436ccd435921a0c3bd9b0e2b7be5
adonay <samueldesta323@gmail.com>
2022-05-26 11:00:58 -0700

    task 2 done

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
-
+/**
+ * add_nodeint - add a new node to a list
+(* a blank line
+*@head: the head of list
+*@n: the integer to put in the new node
+* Description: add a new node to a list)?
+(* section header: the header of this function is lists.h)*
+* Return: the head of the list.
+*/
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
