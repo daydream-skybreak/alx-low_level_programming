32ca696811c3a7c2d0ba0c7b3c6ce21d1e6b62be
adonay <samueldesta323@gmail.com>
2022-06-01 09:39:58 -0700

    task 0 done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - reads text file and prints it in standard output
+ * @filename: a pointer to the file being used
+ * @letters:the number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buf;
+
+	buf = malloc(letters * sizeof(char));
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,6 @@
+## FILE IO
+This is the file io file project that is intended to help us with the knowledge of operating and working wiht files in c
+
+#[A[A[A[[3~[B[B[B[B[C# Author
+Adonay Desta[D[D[D[D[D[D[D[D[D[D[D[DName: Adonay Desta
+Email: samueldesta@gmail.com[D[D[D[D[D[D[D[D[D[D323@gmail.com
