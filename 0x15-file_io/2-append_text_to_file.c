e92faf26cdbfde5ea29a62f8c07f83c7037a1f66
adonay <samueldesta323@gmail.com>
2022-06-03 12:59:46 -0700

    task 2 done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -40,7 +40,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	
 	if (text_content != NULL)
 	{
-		app = write(filename, text_content, length(text_content));
+		app = write(file, text_content, length(text_content));
 
 		if (app == -1)
 		{
