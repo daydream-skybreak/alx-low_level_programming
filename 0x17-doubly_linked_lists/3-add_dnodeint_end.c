a0e34478760868dc68c3afbb867c53431029ad50
Samuel <samueldesta323@gmail.com>
2022-07-08 15:39:11 +0300

    added a semicolon

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,11 +1,15 @@
 #include "lists.h"
 /**
- *
+ * add_dnodeint_end - adds a node to the end of the list
+ * @head: list
+ * @n: value of the node
+ * Return: address of the new element
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *last, *new = malloc(sizeof(dlistint_t));
-
+	dlistint_t *last, *new;
+	`
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
@@ -15,7 +19,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		new->prev = NULL;
-		return (new)
+
+		return (new);
 	}
 
 	last = *head;
