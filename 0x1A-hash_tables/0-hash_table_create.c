e1ccb96e103b44e13ed5fad9b9a46073734fbda1
Samuel <samueldesta323@gmail.com>
2022-07-29 10:27:31 +0300

    improved to betty style

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,20 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
 	unsigned long int i;
+
 	table = malloc(sizeof(hash_table_t));
 
 	if (table == NULL)
-		return NULL;
+		return (NULL);
 
 	table->size = size;
 	table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
-		return NULL;
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
 
-	return table;
+	return (table);
 }
